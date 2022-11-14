785a4e156269699314210468d4063b9e644341ae
Arian <arian.kulmer@web.de>
2022-11-14 21:50:33 +0100

    touchscreen: gt9886: Report fp_state to sysfs
    Change-Id: If6245ce53ed742377ecd2eb53f1debe5fb5a21c8
    Signed-off-by: penglezos <panagiotisegl@gmail.com>
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_gesture.c
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_gesture.c
@@ -377,6 +377,7 @@ static int gsx_gesture_ist(struct goodix_ts_core *core_data,
 		if ((FP_Event_Gesture == 1) && (temp_data[2] == 0x46)) {
 			input_report_key(core_data->input_dev, BTN_INFO, 1);
 			core_data->fod_pressed = true;
+			sysfs_notify(&core_data->gtp_touch_dev->kobj, NULL, "fp_state");
 
 			/*wait for report key event*/
 			FP_Event_Gesture = 0;
@@ -400,6 +401,7 @@ static int gsx_gesture_ist(struct goodix_ts_core *core_data,
 				input_report_key(core_data->input_dev, BTN_INFO, 0);
 				input_sync(core_data->input_dev);
 				core_data->fod_pressed = false;
+				sysfs_notify(&core_data->gtp_touch_dev->kobj, NULL, "fp_state");
 			}
 			core_data->sleep_finger = 0;
 		}
